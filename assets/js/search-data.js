d28e72f8d48110c2d15475101a0c869bba80c926
Sriram-Sai-Ganesh <saiganesh.3@osu.edu>
2025-11-08 21:58:34 +0000

    Deploying to gh-pages from @ Sriram-Sai-Ganesh/sriram-sai-ganesh.github.io@052e9f12272856d5b8dd42e3753526a462979a44 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -480,14 +480,7 @@ ninja.data = [{
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.linkedin.com/in/ram-sai-ganesh", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/sriramsaiganesh", "_blank");
         },
       },{
       id: 'light-theme',
